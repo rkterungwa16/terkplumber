83f22917b14675fc14c0d24206397b06be53baca
rkterungwa16 <terungwakombol@gmail.com>
2023-02-15 15:39:32 +0100

    implement menu button for mobile responsiveness

--- src/components/Page-Header/index.tsx
+++ src/components/Page-Header/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import cx from "classnames";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 import styles from "./styles.module.css";
 
@@ -10,13 +11,19 @@ import { HeaderNavs, ClientRoutes } from "@routes/client";
 import { CustomLink } from "@components/Link";
 import { Button } from "@components/Button";
 import { ButtonColor, ButtonVariant } from "@components/Button/constants";
+import { IconButton } from "@components/Button/icon";
 
 export const PageHeader = () => {
   const router = useRouter();
   const pathname = router?.pathname;
   return (
     <Header className={cx(styles.Page__header, "bg-milky-white")}>
-      <Nav>
+      <Nav className="Trk__dnone--md Trk__absolute--sm Trk__left--3">
+        <IconButton>
+          <Bars3Icon />
+        </IconButton>
+      </Nav>
+      <Nav className="Trk__dflex--sm Trk__justify-center--sm Trk__width-max--sm">
         <CustomLink component={Link} href={ClientRoutes.home}>
           <Image width={117} height={42} src="/logo.png" alt="Logo" />
         </CustomLink>
