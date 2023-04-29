d9e5044bbdfed3376b5b5c52613c16257a54abf8
rkterungwa16 <terungwakombol@gmail.com>
2023-04-29 10:31:46 +0100

    fix build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,25 @@
-import { FC, useState } from "react";
-import { GetServerSideProps, NextApiResponse } from "next";
+import { FC } from "react";
+import { NextApiRequest } from "next";
 import { Home } from "../src/page-components";
 import { Skills } from "types";
 import { FirstSection, SecondSection, ThirdSection } from "types";
 
 type Props = {
   skills: Skills[];
-  data: [FirstSection, SecondSection, ThirdSection] ;
-}
+  data: [FirstSection, SecondSection, ThirdSection];
+};
 
-const HomePage:FC<Props> = ({data}) => {
-
-const { start, name, occupation, summary } = data[0];
-const { secondSectionTitle, details } = data[1];
-const { thirdSectionTitle, skills } = data[2];
+const HomePage: FC<Props> = ({ data }) => {
+  const { start, name, occupation, summary } = data[0];
+  const { secondSectionTitle, details } = data[1];
+  const { thirdSectionTitle, skills } = data[2];
 
   return (
     <>
       <Home
         start={start}
         name={name}
-        occupation= {occupation}
+        occupation={occupation}
         summary={summary}
         secondSectionTitle={secondSectionTitle}
         myDetails={details}
@@ -29,23 +28,29 @@ const { thirdSectionTitle, skills } = data[2];
       />
     </>
   );
-}
+};
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req,
-  params,
-  query,
-}) => {
+export const getServerSideProps = async ({ req }: { req: NextApiRequest }) => {
   try {
     const protocol = process.env.PROTOCOL || "http";
     const host = req?.headers.host;
 
-    const firstRes = (await fetch(`${protocol}://${host}/api/first_section`)).json().then(res => res.data);
-    const secondRes = (await fetch(`${protocol}://${host}/api/second_section`)).json().then(res => res.data);
-    const thirdRes = (await fetch(`${protocol}://${host}/api/third_section`)).json().then(res => res.data);
+    const firstRes = (await fetch(`${protocol}://${host}/api/first_section`))
+      .json()
+      .then((res) => res.data);
+    const secondRes = (await fetch(`${protocol}://${host}/api/second_section`))
+      .json()
+      .then((res) => res.data);
+    const thirdRes = (await fetch(`${protocol}://${host}/api/third_section`))
+      .json()
+      .then((res) => res.data);
 
-    const promises: [Promise<FirstSection>, Promise<SecondSection>, Promise<ThirdSection>] = [firstRes, secondRes, thirdRes];
-    const result = await Promise.all(promises).then((values)=> values);
+    const promises: [
+      Promise<FirstSection>,
+      Promise<SecondSection>,
+      Promise<ThirdSection>
+    ] = [firstRes, secondRes, thirdRes];
+    const result = await Promise.all(promises).then((values) => values);
 
     return {
       props: {
@@ -53,10 +58,8 @@ export const getServerSideProps: GetServerSideProps = async ({
       },
     };
   } catch (error) {
-   console.error("Oops! Something went wrong..."+ error);
+    console.error("Oops! Something went wrong..." + error);
   }
 };
 
-export default  HomePage;
-
-
+export default HomePage;
