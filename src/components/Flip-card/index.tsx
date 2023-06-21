d147ec8661619d1d35825a3ec4633c4a925f094e
rkterungwa16 <terungwakombol@gmail.com>
2023-06-21 19:13:04 +0100

    adjust css change gap to padding

--- src/components/Flip-card/index.tsx
+++ src/components/Flip-card/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState} from "react";
+import { FC, useEffect, useState } from "react";
 import Image from "next/image";
 import { CustomLink } from "@components/Link";
 import cx from "classnames";
@@ -14,64 +14,102 @@ import styles from "./styles.module.css";
 
 type FlipCardProps = {
   skill: Skills;
-}
+};
 
-export const FlipCard:FC<FlipCardProps> = ({skill}) => {
-const { title, icon, competences, iconAlt, details}  = skill;
-  const [ flipped, setFlip ] = useState(false);
+export const FlipCard: FC<FlipCardProps> = ({ skill }) => {
+  const { title, icon, competences, iconAlt, details } = skill;
+  const [flipped, setFlip] = useState(false);
 
   const handleClick = () => {
-    setFlip(current => !current);
-  }
+    setFlip((current) => !current);
+  };
 
-  return(
-    <div className={cx(styles.flip__card)}  >
-      <div className={cx(styles.card__content, flipped === true ? styles.flip__it : "")} >
-          <div className={styles.content__front}>
-            <div className={styles.flipCard__inner}>
-              <Image data-testid="icon-data" src={icon} alt={iconAlt} width={130} height={130}/>
-              <span data-testid="title-data" className={styles.contentFront__title}>{title}</span>
-              <div data-testid="competences-data" className={styles.contentFront__competences}>{competences}</div>
-              <div className={styles.flipCard__detailsLink} onClick={handleClick}>
-                <CustomLink
-                      className={styles.contentFront__details}
-                    >
-                      details
-                </CustomLink>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" strokeWidth={1.5} stroke="#09a311" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                </svg>
-              </div>
+  return (
+    <div className={cx(styles.flip__card)}>
+      <div
+        className={cx(
+          styles.card__content,
+          flipped === true ? styles.flip__it : ""
+        )}
+      >
+        <div className={styles.content__front}>
+          <div className={styles.flipCard__inner}>
+            <Image
+              data-testid="icon-data"
+              src={icon}
+              alt={iconAlt}
+              width={130}
+              height={130}
+            />
+            <span
+              data-testid="title-data"
+              className={styles.contentFront__title}
+            >
+              {title}
+            </span>
+            <div
+              data-testid="competences-data"
+              className={styles.contentFront__competences}
+            >
+              {competences}
             </div>
-
-          </div>
-          <div className={styles.content__back}>
-            <div className={styles.flipCard__inner}>
-              <div className={styles.flipCard__skill__details}>
-                <ul data-testid="details-data">
-                  {
-                    details?.map((detail, idx )=> (
-                      <li key={idx}>
-                        <span>{detail.title}</span>
-                        <p>{detail.stack}</p>
-                      </li>
-                    ))
-                  }
-                </ul>
-              </div>
-              <Button
-                  size={ButtonSize.XS}
-                  color={ButtonColor.WHITE}
-                  variant={ButtonVariant.OUTLINE}
-                  onClick={handleClick}
-                  className={styles.back__buttn}
-                  >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.9} stroke="#09a311" className={styles.backButton__icon}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                  </svg>
-              </Button>
+            <div className={styles.flipCard__detailsLink} onClick={handleClick}>
+              <CustomLink className={styles.contentFront__details}>
+                details
+              </CustomLink>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+                strokeWidth={1.5}
+                stroke="#09a311"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
             </div>
           </div>
+        </div>
+        <div className={styles.content__back}>
+          <div className={styles.flipCard__inner}>
+            <div className={styles.flipCard__skill__details}>
+              <ul data-testid="details-data">
+                {details?.map((detail, idx) => (
+                  <li key={idx}>
+                    <span>{detail.title}</span>
+                    <p>{detail.stack}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <Button
+              size={ButtonSize.XS}
+              color={ButtonColor.WHITE}
+              variant={ButtonVariant.OUTLINE}
+              onClick={handleClick}
+              className={styles.back__buttn}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.9}
+                stroke="#09a311"
+                className={styles.backButton__icon}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Horizontal-scroller/index.tsx
+++ src/components/Horizontal-scroller/index.tsx
@@ -9,49 +9,65 @@ import styles from "./styles.module.css";
 
 type HSProps = {
   children: Skills[];
-}
-export const HorizontalScroller:FC<HSProps> = ({children}) => {
-
+};
+export const HorizontalScroller: FC<HSProps> = ({ children }) => {
   //add a ref to each item element
-  const itemsRef = useRef(Array.from({length: children?.length}, _ => createRef<HTMLLIElement>()));
+  const itemsRef = useRef(
+    Array.from({ length: children?.length }, (_) => createRef<HTMLLIElement>())
+  );
 
   //add a ref to each signal element
-  const signalsRef = useRef(Array.from({length: children?.length}, _ => createRef<HTMLLIElement>()));
+  const signalsRef = useRef(
+    Array.from({ length: children?.length }, (_) => createRef<HTMLLIElement>())
+  );
 
   const listRef = useRef<HTMLUListElement>(null);
 
-const activateSignal = (intersectingIndex: Number) => {
-  signalsRef.current.forEach((signal, i) => {
-    signal.current?.classList.toggle(`${styles['active']}`, i === intersectingIndex)
-  });
-}
+  const activateSignal = (intersectingIndex: Number) => {
+    signalsRef.current.forEach((signal, i) => {
+      signal.current?.classList.toggle(
+        `${styles["active"]}`,
+        i === intersectingIndex
+      );
+    });
+  };
 
- const theInObHook = useIntersectionTracker(itemsRef, listRef.current!, 0.6, activateSignal)
+  const theInObHook = useIntersectionTracker(
+    itemsRef,
+    listRef.current!,
+    0.6,
+    activateSignal
+  );
 
+  useEffect(() => {
+    theInObHook;
+  }, []);
 
-useEffect(() => {
-  theInObHook;
-},[])
-
-  return(
+  return (
     <div className={styles.HS__viewport}>
-      <ul  data-testid="list"  ref={listRef} className={styles.HS__list}>
-        {
-          children?.map((child, idx): ReactElement => (
-            <li className={styles.HS__item} ref={itemsRef.current[idx]} key={idx}>
+      <ul data-testid="list" ref={listRef} className={styles.HS__list}>
+        {children?.map(
+          (child, idx): ReactElement => (
+            <li
+              className={styles.HS__item}
+              ref={itemsRef.current[idx]}
+              key={idx}
+            >
               <FlipCard skill={child} />
             </li>
-          ))
-        }
+          )
+        )}
       </ul>
 
       <ul data-testid="signal" className={styles.HS__signalList} aria-hidden>
-        {
-          Array.from({length: children?.length}, (_, idx) => (
-            <li key={idx} ref={signalsRef.current[idx]} className={cx(styles.HS__signal, "")}></li>
-          ))
-        }
+        {Array.from({ length: children?.length }, (_, idx) => (
+          <li
+            key={idx}
+            ref={signalsRef.current[idx]}
+            className={cx(styles.HS__signal, "")}
+          ></li>
+        ))}
       </ul>
     </div>
   );
-}
+};
