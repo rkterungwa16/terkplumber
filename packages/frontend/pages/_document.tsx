1be9e251c444a31bb843609c5c1e3e140baec423
rkterungwa16 <terungwakombol@gmail.com>
2023-07-12 14:47:43 +0100

    modify responsiveness fix font issue for firefox and safari fix heroicon svg issue with safari fix responsiveness issue in the header and first section

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;900&display=swap"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
